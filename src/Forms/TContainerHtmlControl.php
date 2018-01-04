fe14d1e00453dadf6558d7cf4f3bbc6a22e3fec6
Štěpán Škorpil <stopka@skorpils.cz>
2018-01-04 23:24:40 +0100

    Fixed html component crating

--- src/Forms/TContainerHtmlControl.php
+++ src/Forms/TContainerHtmlControl.php
@@ -9,14 +9,17 @@
 namespace Stopka\NetteFormRenderer\Forms;
 
 
+use Nette\Utils\Html;
+
 trait TContainerHtmlControl {
     /**
      * Adds check box control to the form.
-     * @param  string
-     * @param  string|object
+     * @param string
+     * @param string|object
+     * @param Html $html
      * @return Controls\Html
      */
-    public function addHtml($name, $caption = null) {
-        return $this[$name] = new Controls\Html($caption);
+    public function addHtml($name, $caption = null, Html $html) {
+        return $this[$name] = new Controls\Html($caption,  $html);
     }
 }
